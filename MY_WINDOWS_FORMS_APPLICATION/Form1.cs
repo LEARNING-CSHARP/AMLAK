54b85039ea341fa472b980a672e719bf80089436
Dariush Tasdighi <dariush.tasdighi@hotmail.com>
2019-03-08 21:24:39 +0330

    Making some changes!

--- MY_WINDOWS_FORMS_APPLICATION/Form1.cs
+++ MY_WINDOWS_FORMS_APPLICATION/Form1.cs
@@ -9,7 +9,35 @@
 
 		private void Form1_Load(object sender, System.EventArgs e)
 		{
-			Dtx.Person person = new Dtx.Person();
+			Models.DatabaseContext databaseContext = null;
+
+			try
+			{
+				databaseContext =
+					new Models.DatabaseContext();
+
+				Models.User user = new Models.User();
+
+				user.Username = "Dariush";
+				user.Password = "1234512345";
+				user.FullName = "Mr. Dariush Tasdighi";
+
+				databaseContext.Users.Add(user);
+
+				databaseContext.SaveChanges();
+			}
+			catch (System.Exception ex)
+			{
+				System.Windows.Forms.MessageBox.Show(ex.Message);
+			}
+			finally
+			{
+				if (databaseContext != null)
+				{
+					databaseContext.Dispose();
+					databaseContext = null;
+				}
+			}
 		}
 	}
 }
